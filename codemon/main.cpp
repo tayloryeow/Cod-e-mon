8a340912f9c9135c264dbc0a22eadd4a3c948f77
taylo <taylo@SPD-LARKASCENDA>
2022-11-08 21:47:49 -0500

    Add project files.

---
+++ codemon/main.cpp
@@ -0,0 +1,56 @@
+#include <SFML/Graphics.hpp>
+
+#include <string>
+
+//System is the entire game. keep track of everything made etc.
+class System 
+{
+    
+};
+
+class Window
+{
+private:
+    int px_height, px_width;                            //size of the window  in pixels
+    sf::RenderWindow *win_obj;                          //Text displayed on the screen bar. 
+    std::string display_text;                           //Text displayed on the screen bar. 
+
+public:
+    Window(int width, int height, std::string text) {
+        this->px_height = height;
+        this->px_width = width;
+        
+        this->display_text = text;
+
+
+        //Create Window for the game to take place in; then store  it as a pointer. 
+        sf::RenderWindow window(sf::VideoMode(width, height), this->display_text);
+        this->win_obj = &window;
+    }
+
+    bool is_open() {
+        return (this->win_obj->isOpen());
+    }
+
+    bool change_width(unsigned int width_delta) {
+        this->px_width += width_delta;
+    }
+
+    bool change_height(unsigned int height_delta) {
+        this->px_height += height_delta;
+    }
+
+};
+
+
+int main()
+{
+
+    //Main Screen decleration.  
+    Window scr(200, 200, "Codemon!");
+
+    while (scr.is_open()) {
+
+
+    }
+}
