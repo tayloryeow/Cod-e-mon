c9634decaf95b905e51835f937e5415c7dbb978f
tayloryeow <tayloryeow@gmail.com>
2023-01-02 01:58:50 -0500

    commented in a class introduction

--- codemon/Coordinates.h
+++ codemon/Coordinates.h
@@ -1,4 +1,15 @@
 #pragma once
+
+/*
+Private coordinates tuple class.
+Just a wrapped for x and y variables. I chose to go this direction
+and roll my own just for less dependancies and more flexibility. 
+Additionally rolled my own as an exercise.
+
+Operates on getters setters, etc.. can be created with default or over ridden values.
+Vector addition also is supported
+
+*/
 class Coordinates
 {
 private:
