1bb7dc90e0e9bf26752d94c4ad10470ede3888e5
tayloryeow <tayloryeow@gmail.com>
2023-01-29 02:36:55 -0500

    renamed codemon folder into source. Better reflects actual structure.

---
+++ source/character.cpp
@@ -0,0 +1,221 @@
+#include "SFML/Graphics.hpp"
+
+#include "character.h"
+#include "direction.h"
+#include "Coordinates.h"
+#include "map.h"
+
+#include <cassert>
+
+/* Default Constructor */
+Character::Character() {
+	this->pos = Coordinates(0, 0);
+	this->walk_anim_index = 0;
+	sf::Texture sprite_sheet;
+	sf::Sprite current_sprite;
+	this->facing = DIR::NONE;
+
+	this->sprite_width = 32;
+	this->sprite_height = 32;
+}
+
+// Coordinate Constructor
+Character::Character(int x, int y) {
+	//Create character at a position
+	this->pos = Coordinates(x, y);
+	this->walk_anim_index = 0;
+	sf::Texture sprite_sheet;
+	sf::Sprite current_sprite;
+	this->facing = DIR::NONE;
+
+	this->sprite_width = 32;
+	this->sprite_height = 32;
+
+}
+
+/* Getters Section */
+int Character::get_x()
+{
+	return this->pos.get_x();
+}
+
+int Character::get_y()
+{
+	return this->pos.get_y();
+}
+
+/* Setters section */
+
+void Character::set_x(int new_x)
+{
+	this->pos.set_x(new_x);
+}
+
+void Character::set_y(int new_y)
+{
+	this->pos.set_y(new_y);
+}
+
+/*Move this character by an ammount on the X axis.
+-Set facing direction for the character
+-Move the character
+-No error, bounds or collision checks
+*/
+void Character::move_x(int new_x)
+{
+	//Determine which direction the proposed move (new_x) is in.
+	if (new_x >= 0) {
+		this->set_facing(DIR::E); //Face East
+	}
+	else {
+		this->set_facing(DIR::W); //Face West
+	}
+	//Add the diff to this character's position
+	this->set_x(this->get_x() + new_x);
+}
+
+/*/Move this character byand ammount on the Y axis.
+-Set facing direction for the character
+-Move the character
+-No error, bounds or collision checks
+*/
+void Character::move_y(int new_y)
+{
+	if (new_y >= 0) {
+		this->set_facing(DIR::S);
+	}
+	else {
+		this->set_facing(DIR::N);
+	}
+	//Add the diff to this character's position
+	this->set_y(this->get_y() + new_y);
+}
+
+//Return this character's position
+Coordinates Character::get_pos()
+{
+	return this->pos;
+}
+
+/* Return a pointer to the current sprite. Its basically a getter */
+sf::Sprite* Character::get_current_sprite()
+{
+	//Update sprite
+	return &this->current_sprite;
+}
+
+/* Return a pointer to the entire sprite sheet. Its basically a getter */
+sf::Texture* Character::get_sprite_sheet() {
+	return &this->sprite_sheet;
+}
+
+/* Load entire sprite sheet */
+bool Character::load_sprite_sheet()
+{
+	// Load, into memory, the sprite sheet from a static path (staticness is temp). 
+	// Load into mem from storage.
+	if (!this->sprite_sheet.loadFromFile("assets/Red_player.png")) {
+		return false;
+	}
+
+	// Set the Character's sprite sheet into the obj.
+	this->current_sprite.setTexture(*this->get_sprite_sheet());
+
+	// Set what part of the sprite sheet is displayed.
+	this->current_sprite.setTextureRect(
+		sf::IntRect(
+			0, 0, 
+			this->sprite_width, this->sprite_height
+		)
+	);
+	return true;
+}
+
+//Given a direction, return this character's new position after 
+//a hypothetical move. Does not actually move character.
+Coordinates Character::move_cord(DIR move_dir) {
+	//Probably unneccessary, but good to have while initially setting up 
+	//program
+	assert(move_dir != DIR::NONE);
+
+	//Result holder - A vector tuple that describes how the chararcter would move
+	Coordinates delta(0,0);		
+
+	//Fill result holder with apprioriate vector based on given, desired move direction.
+	switch (move_dir) {
+	case DIR::N:
+		delta = delta + Coordinates(1, 0);
+		break;
+	case DIR::S:
+		delta = delta + Coordinates(-1, 0);
+		break;
+	case DIR::E:
+		delta = delta + Coordinates(0, 1);
+		break;
+	case DIR::W:
+		delta = delta + Coordinates(0, -1);
+		break;
+	}
+
+	//return Tile position that the character would occupy IF they moved
+	return this->get_pos() + delta;
+
+}
+
+//Move character in a direction. 
+//This func actually changes characters position state.
+void Character::move(DIR new_dir)
+{
+	//Make sure its a valid move direction
+	assert(new_dir != DIR::NONE);
+	this->facing = new_dir;
+	switch (new_dir) {
+	case DIR::N:
+		move_y(0 - this->sprite_height);
+		break;
+	case DIR::S:
+		move_y(this->sprite_height);
+		break;
+	case DIR::E:
+		move_x(this->sprite_width);
+		break;
+	case DIR::W:
+		move_x(0 - this->sprite_width);
+		break;
+	}
+}
+
+// Update sprite animation for rendering. 
+// Uses the, hopefully, apprioriately set facing and walk animation values to select which sprite from 
+// the sprite sheet should be shown
+void Character::update_sprite_pos() {
+
+	//Set display target rectangle. 
+	//Relies on, currently:
+	// 1. the walking animation index being the value which describes what part of the walk cycle is.
+	// 2. The relationship between the characters facing enum value 
+	//     being the index for what direction a sprite faces in the sprite sheet 
+	//Essentially as the character walks in a direction, the selected rectangles 
+	//shifts in the X-axis along the 2d spritesheet. Similarily, facing changes the y-axis.
+	sf::IntRect walk_mask = sf::IntRect(
+		walk_anim_index * this->sprite_width, 
+		this->facing * this->sprite_width, 
+		this->sprite_width, this->sprite_height
+	);
+
+	//Update position of cuurent sprite based on the characters position in tile map.
+	//TODO: decouple updating the spritesheet mask and updating a characters position in map.
+	this->current_sprite.setPosition((float) this->get_x(), (float) this->get_y());
+
+	// Set the previously calculated selection rectangle. 
+	this->current_sprite.setTextureRect(walk_mask);
+
+	//Advance the walk cycle.
+	this->walk_anim_index = (this->walk_anim_index + 1) % 3;
+}
+
+/*Getter - updates facing direction to the given direction.*/
+void Character::set_facing(DIR dir)
+{
+	this->facing = dir;
+}
---
+++ source/map.cpp
@@ -0,0 +1,172 @@
+#include "map.h"
+#include "Tile.h"
+#include "Window.h"
+
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cassert>
+
+/* Generic Tile size specification. Works for bare ints or coordinates */
+void Map::set_tile_size(unsigned int tile_width, unsigned int tile_height) {
+    this->tile_dimensions = Coordinates(tile_width, tile_height);
+}
+
+void Map::set_tile_size(Coordinates tile_dimensionality) {
+    this->tile_dimensions = tile_dimensionality;
+}
+
+int Map::get_tile_width() {
+    return this->tile_dimensions.get_x();
+}
+
+int Map::get_tile_height() {
+    return this->tile_dimensions.get_y();
+}
+
+//Create a map object from a given paths to a map file and a tile sheet for said map.
+Map::Map(std::string map_path, std::string sheet_path) {
+    /* Tilesize hardcoding */
+    unsigned int tile_width = 32;
+    unsigned int tile_height = 32;
+
+    /*Initialize all state variables*/
+    this->set_dimensions(Coordinates(0, 0));
+    this->set_start_pos(Coordinates(0, 0));
+    this->set_tile_size(Coordinates(tile_width, tile_height));
+
+    //relative paths to binary map file and sprite sheet image
+    this->map_path = map_path;
+    this->sheet_path = sheet_path;
+    std::fstream mapfile;               //create a spot for map fstream
+
+    /* Load - the sprite sheet for this map. */
+    if (!this->map_sheet.loadFromFile(this->sheet_path))
+    {
+        // TODO: error...
+    }
+
+    /* Load - the integer map textfile for this map */
+    std::string delim = ",";                        //Deliminator for map file
+    mapfile.open(this->map_path, std::ios::in);
+
+    unsigned int x = 0;
+    unsigned int y = 0;
+
+    //if the map_storage file is successfully opened
+    if (mapfile.is_open()) {
+        std::string new_row;
+        std::string tile;
+
+        //Read in height and width data and then create the tile map storage
+        std::getline(mapfile, new_row);
+        this->set_width(unsigned int(new_row[0] - '0'));
+        this->set_height(unsigned int(new_row[2] - '0'));
+
+        //Create the tile storage matrix
+        this->tile_map = new Tile[this->get_width() * this->get_height()];
+
+        //Read in each Tile::enum and create a tile obj for it.
+        //Store said tile in the tile_map
+        while (std::getline(mapfile, new_row)) {
+            std::stringstream tile_row_stream(new_row);
+            //Read in each character for each row
+            while (std::getline(tile_row_stream, tile, ',')) {
+                Tile* new_tile = new Tile(x, y, (Tile::tile)std::stoi(tile));
+                this->add_tile(new_tile);
+                x++;
+            }
+            x = 0;
+            y++;
+        }
+        //Close the input file after reading.
+        mapfile.close();
+    }
+        return;
+}
+
+//Return a texture rendered from get_map();
+//This only works because each tilesheet is constructed such that its value of the enum is its position in the 
+//tile sheet row. i.e. the index of a specific tiles graphics is given by the enum. Pretty dangerous coupling 
+//will change
+void Map::render_map(Window* active_window)
+{
+    //Get the spritesheet read to sample from
+    sf::Sprite to_draw(this->map_sheet);
+    //Read through tile_map and render each tile
+    for (unsigned int y = 0; y < this->get_height(); y++) {             //Each row
+        for (unsigned int x = 0; x < this->get_height(); x++) {         //Each column in each row
+            //Get the tile representation
+            Tile::tile curr_tile = this->tile_map[y * this->get_height() + x].get_data();
+
+            /* Set the appropriate sprite_sheet area to draw */
+            //Calculatex offset of the current tile from the spirite sheet
+            //the tile type id (i.e. the number its saved as) is the index to the spirite sheet
+            int x_sheet_off = ((int)curr_tile) * this->get_tile_width(); 
+            int y_sheet_off = 0; //set as 0 currently. Will need to be extended evetually
+
+            //Clip the tile from the sprite sheet
+            sf::IntRect mask_rect = sf::IntRect(
+                x_sheet_off, y_sheet_off, 
+                this->get_tile_width(), this->get_tile_height()
+            );
+            to_draw.setTextureRect(mask_rect);
+
+            /* Calculate & set the destination coords of the tile for drawing */
+            float dest_x = (float)x * this->get_tile_width();
+            float dest_y = (float)y * this->get_tile_height();
+            to_draw.setPosition(dest_x, dest_y);
+
+            //Draw the current tile to active_windows buffer
+            active_window->draw(&to_draw);
+        }
+    }
+}
+
+//Returns boolean of wether a coordinate is within map boundaries of not.
+//I can skip the lower bounds check that is usually present in a 
+//boundary check as coordinates are defined as unsigned
+bool Map::in_bounds(Coordinates proposed_coord) {
+    return (proposed_coord.get_x() < this->get_width()) && (proposed_coord.get_y() < this->get_height());
+}
+
+/* Getters and Setters section */
+
+void Map::set_dimensions(Coordinates dim) {
+    this->dimensions = dim;
+}
+
+Coordinates* Map::get_dimensions() {
+    return &this->dimensions;
+}
+
+void Map::set_width(unsigned int width) {
+    this->get_dimensions()->set_x(width);
+}
+
+unsigned int Map::get_height() {
+    return this->get_dimensions()->get_y();
+}
+
+void Map::set_height(unsigned int height) {
+    this->get_dimensions()->set_y(height);
+}
+
+unsigned int Map::get_width() {
+    return this->get_dimensions()->get_x();
+}
+
+void Map::set_start_pos(Coordinates start) {
+    this->start_pos = start;
+}
+
+Tile* Map::get_map()
+{
+    return this->tile_map;
+}
+
+void Map::add_tile(Tile* new_tile)
+{
+    this->tile_map[new_tile->get_x() + new_tile->get_y() * this->get_width()] = *new_tile;
+}
